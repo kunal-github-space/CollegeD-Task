a153425540478b560155dafe27d2a93344095bb8
kunal <kunal@eiysys.in>
2020-08-28 14:30:45 +0530

    Updates

---
+++ precache-manifest.cf523a6d1cff54568ed962b496bb07b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d32aa7b03eba1655271fb2cf7adf2d4",
+    "url": "/CollegeD-Task/index.html"
+  },
+  {
+    "revision": "0f3afac77623e8c41ce7",
+    "url": "/CollegeD-Task/static/css/main.eef36548.chunk.css"
+  },
+  {
+    "revision": "2a47375e85c375170b74",
+    "url": "/CollegeD-Task/static/js/2.eafc019a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CollegeD-Task/static/js/2.eafc019a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f3afac77623e8c41ce7",
+    "url": "/CollegeD-Task/static/js/main.c1f3b326.chunk.js"
+  },
+  {
+    "revision": "305e7708dc92d91326bc",
+    "url": "/CollegeD-Task/static/js/runtime-main.52c7859a.js"
+  },
+  {
+    "revision": "8793ec0ee96ac6622dd5064036daac2a",
+    "url": "/CollegeD-Task/static/media/college_01.8793ec0e.jpg"
+  }
+]);
